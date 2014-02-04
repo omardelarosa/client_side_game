3d4cdf2fdd33d5f0eb2a26ba2de524bc67901966
omardelarosa <thedelarosa@gmail.com>
2014-02-04 12:11:23 -0500

    added sortable

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,10 @@
-console.log('feed me javascripts')
+$(function(){
+
+  for (var i = 0; i < 100; i += 1) {
+    $('#photos').append("<li class='photo_item'>photo "+i+"goes here</li>")
+  }
+
+  $('#photos').sortable();
+
+
+})
